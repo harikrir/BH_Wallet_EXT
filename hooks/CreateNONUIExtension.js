2c4815f056a2c22905cee83d6a16cdf16a212bb2
harikrir <hkrishnan254@gmail.com>
2025-12-31 13:02:00 +0300

    Update CreateNONUIExtension.js

--- hooks/CreateNONUIExtension.js
+++ hooks/CreateNONUIExtension.js
@@ -315,12 +315,17 @@ module.exports = function(context) {
 
 
        // --- Start of Capability Fix ---
-var projectFulluuid = pbxProject.projectFulluuid();
+
+var projectFulluuid;
+if (typeof pbxProject.projectFulluuid === 'function') {
+   projectFulluuid = pbxProject.projectFulluuid();
+} else {
+   projectFulluuid = pbxProject.projectFulluuid;
+}
 var projectAttributes = pbxProject.getPBXProject().objs.PBXProject[projectFulluuid].attributes;
 if (!projectAttributes.TargetAttributes) {
    projectAttributes.TargetAttributes = {};
 }
-// List both possible target names to be safe
 var targetNames = ['WalletExtension', 'WalletExtensionUI'];
 targetNames.forEach(function(name) {
    var targetKey = pbxProject.findTargetKey(name);
